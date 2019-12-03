ac3a78da5c94509635fbc18490f71da862374ed3
nguyenkhanhtung <tungnk0711@gmail.com>
2019-12-03 15:18:04 +0700

    update

--- src/com/codegym/dao/IUserDAO.java
+++ src/com/codegym/dao/IUserDAO.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface IUserDAO {
+
     public void insertUser(User user) throws SQLException;
 
     public User selectUser(int id);
@@ -15,4 +16,8 @@ public interface IUserDAO {
     public boolean deleteUser(int id) throws SQLException;
 
     public boolean updateUser(User user) throws SQLException;
+
+    public User getUserById(int id);
+
+    public void insertUserStore(User user) throws SQLException;
 }
